0e0288568caae66728845799dc57069f90a47aff
ArthurSilveiraRocha <arthur-rocha735@outlook.com>
2025-11-10 19:42:20 -0300

    Update 01_create_tables.sql

---
+++ db/01_create_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS paises(
+    id_pais serial primary key,
+    sigla char(3),
+    nome varchar(100) not null
+);
+
+CREATE TABLE IF NOT EXISTS equipes(
+    id_equipe serial primary key,
+    nome varchar(100) not null unique,
+    id_pais int references paises(id_pais)
+);
+
+CREATE TABLE IF NOT EXISTS pilotos(
+    id_piloto serial primary key,
+    nome varchar(100) not null,
+    id_pais references paises(id_pais)
+);
+
+CREATE TABLE pilotos_equipes (
+    id_equipe int references equipes(id_equipe)
+    id_piloto int references pilotos(id_piloto)
+    data_inicio date not null,
+    
+);
+
