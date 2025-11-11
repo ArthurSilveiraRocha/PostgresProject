4c09239a9cc2ac599643cb54f60ed11ca8a75c5f
ArthurSilveiraRocha <arthur-rocha735@outlook.com>
2025-11-11 00:51:21 -0300

    Update 04_alter_tables.sql

--- db/04_alter_tables.sql
+++ db/04_alter_tables.sql
@@ -1,4 +1,5 @@
-/* Atributo UNIQUE adicionado para que os inserts
+/*
+Atributo UNIQUE adicionado para que os inserts
 possam ser rodados individualmente ou todos ao mesmo
 tempo na mesma query. Como usei o VSCode para
 criação do banco, gostaria de salvar todos os inserts
@@ -7,7 +8,10 @@ rodados novamente (por ser um mesmo script), não impedissem
 que os novos inserts rodassem.
 Vou adicionar o link do
 repositório Git só por vias de curiosidade, caso o professor
-desejar conferir posteriormente */
+desejar conferir posteriormente
+
+https://github.com/ArthurSilveiraRocha/PostgresProject.git
+*/
 
 ALTER TABLE paises
     ADD CONSTRAINT uk_paises_sigla UNIQUE (sigla);
@@ -23,6 +27,14 @@ ALTER TABLE circuitos
 
 ALTER TABLE corridas
     ADD CONSTRAINT uk_corridas_data_nome UNIQUE (date_corrida, nome_corrida);
+
+/*Como a tabela pilotos_equipes já possuia uma primary key, removi a mesma
+para padronizar os atributos UNIQUE da tabela e formalizar
+o uso do método ON CONFLICT DO NOTHING nos inserts*/
+
+
+ALTER TABLE pilotos_equipes
+    DROP CONSTRAINT IF EXISTS pilotos_equipes_pkey;
 
 ALTER TABLE pilotos_equipes
     ADD CONSTRAINT pk_pilotos_equipes PRIMARY KEY (id_equipe, id_piloto, data_inicio);
